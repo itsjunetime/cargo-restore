cf95b7c1df6e93da8106562e7b5042a2f3e3f43e
itsjunetime <junewelker@gmail.com>
2024-01-15 18:22:36 -0700

    Reformat with hard_tabs and fix issue of same config being used between installs (caused some weird failures)

--- src/config.rs
+++ src/config.rs
@@ -4,17 +4,17 @@ use std::path::PathBuf;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Config {
-    /// The file to parse as the .crates2.json if you don't want to use the default one
-    #[arg(short = 'c', long = "crate-file")]
-    pub crates_file: Option<PathBuf>,
-    /// Change the target of all to-be-installed packges to match this device's target if it's different
-    #[arg(short = 'f', long = "fix-target", default_value_t = true)]
-    pub fix_target: bool,
-    /// Whether to install the latest version available (true) or install the version in the
-    /// lockfile (false, default)
-    #[arg(short = 'i', long = "install-latest", default_value_t = false)]
-    pub install_latest: bool,
-    /// Whether to enable verbose output
-    #[arg(short = 'v', long = "verbose", default_value_t = false)]
-    pub verbose: bool,
+	/// The file to parse as the .crates2.json if you don't want to use the default one
+	#[arg(short = 'c', long = "crate-file")]
+	pub crates_file: Option<PathBuf>,
+	/// Change the target of all to-be-installed packges to match this device's target if it's different
+	#[arg(short = 'f', long = "fix-target", default_value_t = true)]
+	pub fix_target: bool,
+	/// Whether to install the latest version available (true) or install the version in the
+	/// lockfile (false, default)
+	#[arg(short = 'i', long = "install-latest", default_value_t = false)]
+	pub install_latest: bool,
+	/// Whether to enable verbose output
+	#[arg(short = 'v', long = "verbose", default_value_t = false)]
+	pub verbose: bool,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use cargo::{
-    core::{compiler::CompileKind, resolver::CliFeatures, FeatureValue},
-    ops::{install, CompileFilter, CompileOptions, FilterRule, LibRule, Packages},
-    util::{command_prelude::CompileMode, interning::InternedString},
+	core::{compiler::CompileKind, resolver::CliFeatures, FeatureValue},
+	ops::{install, CompileFilter, CompileOptions, FilterRule, LibRule, Packages},
+	util::{command_prelude::CompileMode, interning::InternedString},
 };
 use clap::Parser;
 use semver::{Comparator, VersionReq};
@@ -11,83 +11,87 @@ mod config;
 mod crates;
 
 fn main() {
-    // todo)) add like tracing and stuff to make sure it looks pretty and only outputs the
-    // necessary info
-    let config = config::Config::parse();
+	// todo)) add like tracing and stuff to make sure it looks pretty and only outputs the
+	// necessary info
+	let config = config::Config::parse();
 
-    let crates = crates::load_info(&config).expect("Couldn't load list of crates");
-    let listing = crates.listing.get();
-    let bin_dir = crates.root.clone().join("bin");
+	let crates = crates::load_info(&config).expect("Couldn't load list of crates");
+	let listing = crates.listing.get();
+	let bin_dir = crates.root.clone().join("bin");
 
-    for (package, info) in listing.installs.iter() {
-        // todo)) sometimes the version can be like 0.1.0-master and the `master` is only contained
-        // in the `semver::Version`, but i don't know if we can translate that over to the
-        // `semver::VersionReq`. maybe it'll be fine.
-        let vers = (!config.install_latest).then(|| {
-            let pkg_vers = package.version();
-            VersionReq {
-                comparators: vec![Comparator {
-                    op: semver::Op::Exact,
-                    major: pkg_vers.major,
-                    minor: Some(pkg_vers.minor),
-                    patch: Some(pkg_vers.patch),
-                    pre: pkg_vers.pre.clone(),
-                }],
-            }
-        });
+	for (package, info) in listing.installs.iter() {
+		// We want to recreate this with every package because it seems that if you use it with
+		// multiple installs it can get messed up and affect later installs
+		let cargo_config = cargo::Config::default().expect("Couldn't create cargo Config");
 
-        let mut opts = CompileOptions::new(&crates.config, CompileMode::Build)
-            .expect("Couldn't create compile opts");
+		// todo)) sometimes the version can be like 0.1.0-master and the `master` is only contained
+		// in the `semver::Version`, but i don't know if we can translate that over to the
+		// `semver::VersionReq`. maybe it'll be fine.
+		let vers = (!config.install_latest).then(|| {
+			let pkg_vers = package.version();
+			VersionReq {
+				comparators: vec![Comparator {
+					op: semver::Op::Exact,
+					major: pkg_vers.major,
+					minor: Some(pkg_vers.minor),
+					patch: Some(pkg_vers.patch),
+					pre: pkg_vers.pre.clone(),
+				}],
+			}
+		});
 
-        if config.fix_target {
-            opts.build_config.requested_kinds = vec![CompileKind::Host];
-        }
+		let mut opts = CompileOptions::new(&cargo_config, CompileMode::Build)
+			.expect("Couldn't create compile opts");
 
-        opts.build_config.requested_profile = InternedString::new(info.profile);
-        opts.cli_features = CliFeatures {
-            features: Rc::new(BTreeSet::from_iter(
-                info.features
-                    .iter()
-                    .map(|feat| FeatureValue::Feature(InternedString::new(feat))),
-            )),
-            all_features: info.all_features,
-            uses_default_features: !info.no_default_features,
-        };
+		if config.fix_target {
+			opts.build_config.requested_kinds = vec![CompileKind::Host];
+		}
 
-        let packages = info.bins.iter().map(|s| s.to_string()).collect::<Vec<_>>();
+		opts.build_config.requested_profile = InternedString::new(info.profile);
+		opts.cli_features = CliFeatures {
+			features: Rc::new(BTreeSet::from_iter(
+				info.features
+					.iter()
+					.map(|feat| FeatureValue::Feature(InternedString::new(feat))),
+			)),
+			all_features: info.all_features,
+			uses_default_features: !info.no_default_features,
+		};
 
-        opts.spec = Packages::Packages(packages);
-        opts.filter = CompileFilter::Only {
-            all_targets: false,
-            lib: LibRule::Default,
-            bins: FilterRule::All,
-            examples: FilterRule::Just(vec![]),
-            tests: FilterRule::Just(vec![]),
-            benches: FilterRule::Just(vec![]),
-        };
+		let packages = info.bins.iter().map(|s| s.to_string()).collect::<Vec<_>>();
 
-        // force_build should be true if any of the bins don't exist so that cargo doesn't stop us
-        // when it sees that the pacakge is already installed according to the lockfile
-        let force_build = info.bins.iter().any(|bin| {
-            bin_dir
-                .open_ro_shared(bin, &crates.config, "Checking if binary exists")
-                .map_or(true, |file| !file.path().exists())
-        });
+		opts.spec = Packages::Packages(packages);
+		opts.filter = CompileFilter::Only {
+			all_targets: false,
+			lib: LibRule::Default,
+			bins: FilterRule::All,
+			examples: FilterRule::Just(vec![]),
+			tests: FilterRule::Just(vec![]),
+			benches: FilterRule::Just(vec![]),
+		};
 
-        // todo)) find a way to make this quieter
-        let res = install(
-            &crates.config,
-            None,
-            vec![(package.name().as_str().into(), vers)],
-            package.source_id(),
-            false,
-            &opts,
-            force_build,
-            false,
-        );
+		// force_build should be true if any of the bins don't exist so that cargo doesn't stop us
+		// when it sees that the pacakge is already installed according to the lockfile
+		let force_build = info.bins.iter().any(|bin| {
+			bin_dir
+				.open_ro_shared(bin, &cargo_config, "Checking if binary exists")
+				.map_or(true, |file| !file.path().exists())
+		});
 
-        if let Err(e) = res {
-            eprintln!("Couldn't install {}: {e}", package.name());
-        }
-    }
+		// todo)) find a way to make this quieter
+		let res = install(
+			&cargo_config,
+			None,
+			vec![(package.name().as_str().into(), vers)],
+			package.source_id(),
+			false,
+			&opts,
+			force_build,
+			false,
+		);
+
+		if let Err(e) = res {
+			eprintln!("Couldn't install {}: {e}", package.name());
+		}
+	}
 }
